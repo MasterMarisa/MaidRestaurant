189cee385c1b5d0a3607bbaa884666fe89485549
Jaoreir <jaoreir@gmail.com>
2026-02-18 12:38:47 +0800

    Fix distribution tick count so every dimension gets ticked properly

--- src/main/java/com/mastermarisa/maid_restaurant/event/RequestDistributor.java
+++ src/main/java/com/mastermarisa/maid_restaurant/event/RequestDistributor.java
@@ -8,12 +8,12 @@ import net.neoforged.neoforge.event.tick.LevelTickEvent;
 
 @EventBusSubscriber
 public class RequestDistributor {
-    private static long serverTickCount = 0;
+    private static int ticksPerDistribution = 10;
 
     @SubscribeEvent
     public static void onLevelTickPre(LevelTickEvent.Pre event) {
-        if (event.getLevel().isClientSide()) return;
-        serverTickCount++;
-        if (serverTickCount % 10 == 0) RequestManager.tryDistributeRequests((ServerLevel) event.getLevel());
+        if(!(event.getLevel() instanceof ServerLevel serverLevel)) return;
+        if(serverLevel.getGameTime() % ticksPerDistribution != 0 )return;
+        RequestManager.tryDistributeRequests(serverLevel);
     }
 }
